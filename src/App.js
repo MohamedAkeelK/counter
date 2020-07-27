c173dab87dd74f119c73ca48e7897bfd54ff1be0
unknown <Mohamedakeelkhan@gmail.com>
2020-07-27 16:17:44 -0400

    added increment functionality to the increment button

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ class Counter extends Component {
      count: 0,
    };
  }
+
+ increment = () => {
+   this.setState ({
+     count: this.state.count + 1
+   })
+ }
  
   render() {
     return(
-      <div></div>
+      <div className = "container">
+        <div className = "navbar">Counter.js</div>
+        <div className = "counter">
+          <h1>{this.state.count}</h1>
+          <button type = "button" onClick = {this.increment}>Increment</button>
+          <button type = "button">Decrement</button>
+        </div>
+      </div>
     )
   }
 }
